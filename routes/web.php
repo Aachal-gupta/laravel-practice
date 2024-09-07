07d429216c616db2bac02cc0dfd44ebed999666f
Aachal-gupta <aachalgupta8080@gmail.com>
2024-09-07 16:58:50 +0530

    routing parameters

--- routes/web.php
+++ routes/web.php
@@ -5,9 +5,17 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {    //this is home page
     return view('welcome');
 });
-
-Route::get('/demo', function () {
+                                    // on server in terminal by this cmd php artisan serve
+Route::get('/demo', function () {    //put on url http://localhost:8000/demo
     return view('about');
+});
+
+Route::get('/pest/{id?}', function (string $id="not defined"){     // take on url http://localhost:8000/pest/22
+    if($id){
+        return "<h2>In url ID = ". $id ."</h2>";
+    }else{
+        return "<h1>In url id is not taken .</h1>";
+    }
 });
 
 
