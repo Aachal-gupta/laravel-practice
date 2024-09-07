0631d4347f9199f84b971c3255fa14a88bdcd57f
Aachal-gupta <aachalgupta8080@gmail.com>
2024-09-07 18:50:01 +0530

    route constraint

--- routes/web.php
+++ routes/web.php
@@ -10,12 +10,22 @@ Route::get('/demo', function () {    //put on url http://localhost:8000/demo
     return view('about');
 });
 
-Route::get('/pest/{id?}', function (string $id="not defined"){     // take on url http://localhost:8000/pest/22
+// it is default parameter
+// Route::get('/pest/{id?}', function (string $id="not defined"){     //  take on url http://localhost:8000/pest/22
+//     if($id){
+//         return "<h2>In url ID = ". $id ."</h2>";
+//     }else{
+//         return "<h1>In url id is not taken .</h1>";
+//     }
+// });
+
+// Route constraint
+// http://localhost:8000/pest/aachal
+Route::get('pest/{id}', function(string $id){
     if($id){
-        return "<h2>In url ID = ". $id ."</h2>";
-    }else{
-        return "<h1>In url id is not taken .</h1>";
+        return "<h1> the ID = ". $id ."</h1>";
     }
-});
-
-
+    else{
+        return "<h2> NOT FOUND ID</h2>";
+    }
+})->whereAlpha('id');
